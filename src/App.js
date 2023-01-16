e47cf6c46abe28306cad2bee2b761e9335d60b1b
nmtuyen316 <95425898+nmtuyen316@users.noreply.github.com>
2023-01-16 14:06:37 +0700

    setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import { Canvas } from '@react-three/fiber';
+import {Suspense} from 'react'
 import './App.css';
-
+import AllRoutes from './AllRoutes/AllRoutes';
+import { OrbitControls, Stars } from '@react-three/drei';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas id='three-canvas-container'>
+      <Suspense fallback={null}>
+        <Stars/>
+        <OrbitControls/>
+        <AllRoutes />
+      </Suspense>
+    </Canvas>
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () =>{
+    return (
+        <mesh>
+            <boxBufferGeometry attach={'geometry'}/>
+            <meshLambertMaterial attach={'material'} color={'lightblue'}/>
+        </mesh>
+    )
+}
+export default Home
